4071f6a104d372aa4294e5d5560235f2f1f81c8b
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-04-03 13:10:24 -0500

    Download to mp4

--- main.go
+++ main.go
@@ -3,23 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/kkdai/youtube/v2"
+	"io"
+	"os"
 )
 
 func main() {
 	client := youtube.Client{}
 	var url string
 
-	isError := true
-	for isError {
-		fmt.Print("Enter the url: ")
-		fmt.Scan(&url)
-		video, err := client.GetVideo(url)
-		if err != nil {
-			fmt.Println("There was an error: ", err)
-			continue
-		}
-		isError = false
-		fmt.Println(video.Title)
+	fmt.Print("Enter the url: ")
+	fmt.Scan(&url)
+	video, err := client.GetVideo(url)
+	if err != nil {
+		fmt.Println("There was an error: ", err)
+	}
+
+	fmt.Println("Title: ", video.Title)
+	fmt.Println("Description: ", video.Description)
+	fmt.Println("Duration: ", video.Duration)
+
+	formats := video.Formats.WithAudioChannels()
+
+	stream, _, err := client.GetStream(video, &formats[0])
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create("video.mp4")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		panic(err)
 	}
 
 	fmt.Println("Hello, World!")
